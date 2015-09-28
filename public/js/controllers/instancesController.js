f4b3c6376d32ae0d593ba10f4937e80f59e7c273
pradiphudekar <pradiphudekar@gmail.com>
2015-09-28 17:37:51 +0530

    adding start stop buttons

--- public/js/controllers/instancesController.js
+++ public/js/controllers/instancesController.js
@@ -4,4 +4,24 @@ app.controller("InstancesController", ["instances", "organizations", function (i
 	self.instances = instances;
 	self.organizations = organizations;
 
+	self.start = function () {
+		var updatedInstances = _.map(self.instances, function (instance) {
+			if (instance.organization == self.currentOrganization && instance.isSelected) {
+				instance.state = 'running';
+			}
+			return instance;
+		});
+		self.instances = updatedInstances;
+	};
+
+	self.stop = function () {
+		var updatedInstances = _.map(self.instances, function (instance) {
+			if (instance.organization == self.currentOrganization && instance.isSelected) {
+				instance.state = 'stopped';
+			}
+			return instance;
+		});
+		self.instances = updatedInstances;
+	};
+
 }]);
