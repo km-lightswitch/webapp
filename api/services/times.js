b389bdf94251792b4da7b2cc78bd5bffd29a301e
Krishnan Mani <km@krishnanm.com>
2015-10-06 00:53:04 +0530

    Added more functionality and tests to Times module

--- api/services/times.js
+++ api/services/times.js
@@ -1,13 +1,49 @@
 var moment = require('moment-timezone');
 
-var toTimeOfDay = function toTimeOfDay(hhmmStr, tzStr) {
+var timeOfDay = function timeOfDay(str) {
+	if ((str.split(':')).length != 2) {
+		throw new TypeError("Invalid hh:mm string");
+	}
+
+	var hours = parseInt(str.split(':')[0]);
+	if (hours < 0 || hours > 24) {
+		throw new TypeError("Invalid hour " + hours);
+	}
+
+	var minutes = parseInt(str.split(':')[1]);
+	if (minutes < 0 || minutes > 59) {
+		throw new TypeError("Invalid minute " + minutes);
+	}
+
+	return ((hours * 60) + minutes);
 };
+
+var minutesInADay = 24 * 60;
+
+var timeOfDayOffset = function timeOfDayOffset(str, offset) {
+	var tzPlus = offset[0];
+	var tzOffsetMinutes = timeOfDay(offset.slice(1));
+	var timeOfDay = timeOfDay(str);
+	if (tzPlus == '+') {
+		var calculated = timeOfDay - tzOffsetMinutes;
+		return (calculated < 0 ?
+			{ "day": -1, "time": (calculated + minutesInADay) }
+			: { "day": 0, "time": calculated });
+	} else {
+		var calculated = timeOfDay + tzOffsetMinutes;
+		return (calculated > minutesInADay ?
+			{ "day": 1, "time": (calculated - minutesInADay) }
+			: { "day": 0, "time": calculated });
+	}
+};
+
 
 var toUTC = function toUTC(timeStr) {
 	return moment(timeStr).utc();
 };
 
 module.exports = {
-	toTimeOfDay: toTimeOfDay,
+	timeOfDay: timeOfDay,
+	timeOfDayOffset: timeOfDayOffset,
 	toUTC: toUTC
 };
--- test/testTimes.js
+++ test/testTimes.js
@@ -1,11 +1,65 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
 var moment = require('moment-timezone');
 var times = require('../api/services/times.js');
 
-describe('Times', function(){
-	describe('#toUTC()', function(){
-		it ('converts a time in a specified timezone to corresponding UTC time', function(){
+describe('Times', function () {
+	describe('#toUTC()', function () {
+		it('converts a time in a specified timezone to corresponding UTC time', function () {
 			expect(times.toUTC("2015-11-05T17:40:00+05:30").format()).to.equal("2015-11-05T12:10:00+00:00");
+		});
+	})
+});
+
+describe('Times', function () {
+	describe('#timeOfDay()', function () {
+		it('converts an hh:mm string to a time of day in minutes since midnight', function () {
+			expect(times.timeOfDay("01:23")).to.equal(83);
+			expect(times.timeOfDay("12:21")).to.equal((12 * 60) + 21);
+		});
+
+		it('complains when an invalid format string is passed in', function () {
+			//SHITTY WORKAROUND, SINCE expect(fn).to.throw(Error, /message/) is not working!
+			var caughtError = false;
+			try {
+				times.timeOfDay("23");
+			} catch (err) {
+				caughtError = true;
+				expect(caughtError).to.be.true;
+			}
+
+			caughtError = false;
+			try {
+				times.timeOfDay("25:03");
+			} catch (err) {
+				caughtError = true;
+				expect(caughtError).to.be.true;
+			}
+
+			caughtError = false;
+			try {
+				times.timeOfDay("-5:03");
+			} catch (err) {
+				caughtError = true;
+				expect(caughtError).to.be.true;
+			}
+
+			caughtError = false;
+			try {
+				times.timeOfDay("21:-3");
+			} catch (err) {
+				caughtError = true;
+				expect(caughtError).to.be.true;
+			}
+
+			caughtError = false;
+			try {
+				times.timeOfDay("21:60");
+			} catch (err) {
+				caughtError = true;
+				expect(caughtError).to.be.true;
+			}
 		});
 	})
 });
